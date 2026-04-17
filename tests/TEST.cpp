f5b411051f47aa6b57d35e319d289d4b94a84520
djkzn <zotov2007@gmail.com>
2026-04-17 23:22:39 +0300

    fix: correct TEST.cpp using real Account API

--- tests/TEST.cpp
+++ tests/TEST.cpp
@@ -8,10 +8,29 @@ TEST(AccountTest, InitialBalance) {
     EXPECT_EQ(a.GetBalance(), 100);
 }
 
-TEST(AccountTest, Deposit) {
-    Account a(1, 0);
-    a.SetBalance(a.GetBalance() + 50);
-    EXPECT_EQ(a.GetBalance(), 50);
+TEST(AccountTest, ChangeBalance) {
+    Account a(1, 100);
+    a.Lock();
+    a.ChangeBalance(50);
+    EXPECT_EQ(a.GetBalance(), 150);
+    a.Unlock();
+}
+
+TEST(AccountTest, Id) {
+    Account a(42, 0);
+    EXPECT_EQ(a.id(), 42);
+}
+
+TEST(AccountTest, LockThrows) {
+    Account a(1, 100);
+    a.Lock();
+    EXPECT_THROW(a.Lock(), std::runtime_error);
+    a.Unlock();
+}
+
+TEST(AccountTest, ChangeBalanceWithoutLockThrows) {
+    Account a(1, 100);
+    EXPECT_THROW(a.ChangeBalance(50), std::runtime_error);
 }
 
 int main(int argc, char **argv) {
