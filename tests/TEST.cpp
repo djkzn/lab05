16632f70d5150cb6ce670e899438c72b6e66c9a2
djkzn <zotov2007@gmail.com>
2026-04-21 17:12:35 +0300

    fix TEST.cpp

--- tests/TEST.cpp
+++ tests/TEST.cpp
@@ -150,7 +150,52 @@ TEST(TransactionMockTest, MakeRollsBackCreditOnInsufficientFunds) {
     bool result = t.Make(from, to, 200);
     EXPECT_FALSE(result);
 }
+TEST(AccountTest, UnlockAllowsLockAgain) {
+    Account a(1, 100);
+    a.Lock();
+    a.Unlock();
+    EXPECT_NO_THROW(a.Lock());
+    a.Unlock();
+}
 
+TEST(TransactionRealTest, SuccessfulTransferWithRealAccounts) {
+    Account from(1, 1000);
+    Account to(2, 0);
+    Transaction t;
+    t.set_fee(1);
+
+    bool result = t.Make(from, to, 200);
+
+    EXPECT_TRUE(result);
+    EXPECT_EQ(from.GetBalance(), 1000);
+    EXPECT_EQ(to.GetBalance(), 199);
+}
+
+TEST(TransactionRealTest, FailedTransferRollsBackCreditWithRealAccounts) {
+    Account from(1, 1000);
+    Account to(2, 0);
+    Transaction t;
+    t.set_fee(200);
+
+    bool result = t.Make(from, to, 300);
+
+    EXPECT_FALSE(result);
+    EXPECT_EQ(from.GetBalance(), 1000);
+    EXPECT_EQ(to.GetBalance(), 0);
+}
+
+TEST(TransactionRealTest, BorderCaseWhenBalanceEqualsDebitSumReturnsFalse) {
+    Account from(1, 1000);
+    Account to(2, 0);
+    Transaction t;
+    t.set_fee(100);
+
+    bool result = t.Make(from, to, 100);
+
+    EXPECT_FALSE(result);
+    EXPECT_EQ(from.GetBalance(), 1000);
+    EXPECT_EQ(to.GetBalance(), 0);
+}
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
